63cc7ff7f2c1e42f1de7b43c3453b5c9ab5bbbc0
ZAKARYA123J <118215217+ZAKARYA123J@users.noreply.github.com>
2023-12-11 00:32:59 +0100

    yes

--- src/compent/home.jsx
+++ src/compent/home.jsx
@@ -3,18 +3,7 @@ import ImageWithText from './service';
 import { Container, Typography, Button } from '@mui/material';
 
 const Home = () => {
-  const imageInfoArray = [
-    {
-      imageUrl: 'https://www.netzpiloten.de/wp-content/uploads/2019/06/adobestock_sergey_coding_lernen_titelbild.jpg',
-      title: 'programation web',
-      description: 'Enhance your online presence with our cutting-edge Web Programming Service. Our team of skilled developers is dedicated to crafting seamless and innovative web solutions tailored to meet your unique needs. From front-end development, creating stunning user interfaces, to back-end functionality ensuring robust performance, we cover the entire spectrum of web programming.',
-    },
-    {
-      imageUrl: 'https://www.saveeat.co/wp-content/uploads/2020/02/attirer-les-clients.jpg',
-      title: 'SEO',
-      description: 'Elevate your online visibility and drive organic traffic with our comprehensive SEO Service. In todays digital landscape, being easily discoverable on search engines is imperative for business success. Our dedicated team of SEO experts is committed to optimizing your online presence, ensuring that your website ranks prominently on search engine results pages.',
-    },
-  ];
+ 
   return (
     <Container maxWidth="md" sx={{ mt: 4 }}>
       <Typography variant="h2" component="div" gutterBottom>
@@ -39,14 +28,7 @@ const Home = () => {
         Learn More
       </Button>
       <br/>
-      {imageInfoArray.map((imageInfo, index) => (
-        <ImageWithText
-          key={index}
-          imageUrl={imageInfo.imageUrl}
-          title={imageInfo.title}
-          description={imageInfo.description}
-        />
-      ))}
+    
     </Container>
 
   );
